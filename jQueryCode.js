91da50c5dd697aea531e31f93dd3826caf9f968a
Forzana <60197480+forzana@users.noreply.github.com>
2022-01-05 16:26:06 -0500

    Update jQueryCode.js

--- jQueryCode.js
+++ jQueryCode.js
@@ -1,1 +1,8 @@
+$().ready(function() {
+	$("#text").html("Text added by jQuery code.");
 
+	$.get("https://dev-rhodes.embark.com/embeddedProspects/form/marshall/registration2022", function( data ) {
+		console.log(data);
+		$("#inquiryForm").html(data);
+	});
+});
